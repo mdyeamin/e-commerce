f10981c9cec1faea39815f010660935233efe361
md yeamin <ahmedyeamin05@gmail.com>
2025-02-21 19:19:48 +0600

    navbar init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   theme: {
+    colors: {
+        header_primary: "#048CC3",
+        e_commarce_red: "red"
+      },
     extend: {
       fontFamily: {
         sans: ['InterVariable', '...defaultTheme.fontFamily.sans'],
